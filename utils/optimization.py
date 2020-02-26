1e7780cba88570e09b2cea2f366a2d9545e63d9e
Tony Cheng <tcheng102@gmail.com>
2020-02-25 23:09:19 -0600

    added optimization for ensemble.

--- utils/optimization.py
+++ utils/optimization.py
@@ -115,5 +115,56 @@ def AMN_perc_optimization(AMN_net, expert_net, optimizer, memory, feature_regres
     return loss
 
 
+def _AMN_optimization_ENS(AMN_net, expert_net, optimizer, state_batch, ens_num=None, feature_regression=False, tau=0.1,
+                          beta=0.01, GAMMA=0.99, training=True):
+    """
+    Apply the standard procedure to deep Q network.
+    """
+    if not training:
+        return None
+
+    if feature_regression == True:
+        AMN_q_value, AMN_last_layer = AMN_net(state_batch, last_layer=True)
+        expert_q_value, expert_last_layer = expert_net(
+            state_batch, last_layer=True)
+        loss = F.mse_loss(AMN_last_layer, expert_last_layer.detach())
+    else:
+        AMN_q_value = AMN_net(state_batch, ens_num=ens_num, last_layer=False)
+        expert_q_value = expert_net(state_batch, last_layer=False)
+        loss = 0
+
+    AMN_policy = to_policy(AMN_q_value)
+    expert_policy = to_policy(expert_q_value).detach()
+
+    loss -= torch.sum(expert_policy * torch.log(AMN_policy + 1e-8))
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss.detach()
+
+
+def AMN_perc_optimization_ENS(AMN_net, expert_net, optimizer, memory, feature_regression=False, tau=0.1,
+                              percentage=0.1, beta=0.01, batch_size=256, GAMMA=0.99, training=True,
+                              device='cuda'):
+    if not training:
+        return None
+
+    bs, _, _, _, _ = memory.sample(percentage)
+    bs_len = bs.shape[0]
+    loss = 0
+    for i in range(0, bs_len, batch_size):
+        for ens_num in range(5):
+            if i + batch_size < bs_len:
+                actual_batch_size = batch_size
+            else:
+                actual_batch_size = bs_len - i
+            next_bs = bs[i: i + actual_batch_size].to(device)
+            loss += _AMN_optimization_ENS(AMN_net, expert_net, optimizer, next_bs, ens_num=ens_num, feature_regression=feature_regression,
+                                          tau=tau, beta=beta, GAMMA=GAMMA, training=training)
+    return loss
+
+
 def to_policy(q_values, tau=0.1):
     return F.softmax(q_values / tau, dim=1)
