bd0c5f1765b894c16d10bb318d3effe68fb42838
Tony Cheng <tcheng102@gmail.com>
2020-03-15 13:11:01 -0500

    moved most files into a package.

---
+++ active_rl/networks/__init__.py
@@ -0,0 +1,1 @@
+from .dqn_atari import DQN
--- active_rl/networks/dqn_atari.py
+++ active_rl/networks/dqn_atari.py
@@ -1,8 +1,6 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-import random
-import torch.optim as optim 
 
 
 class DQN(nn.Module):
@@ -123,6 +121,7 @@ class ENS_DQN(nn.Module):
     def eval(self):
         for i in range(len(self.ensembles)):
             self.ensembles[i].eval()
+
 
 def to_policy(q_values, tau=0.1):
     return F.softmax(q_values / tau, dim=1)
