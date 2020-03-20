70105e055edb6e7aa7bdd940dc8e8dc2c603e2d3
Tony Cheng <tcheng102@gmail.com>
2020-03-20 02:54:26 -0500

    fixed another bug.

--- active_rl/experiments_template/standard_atari.py
+++ active_rl/experiments_template/standard_atari.py
@@ -7,11 +7,20 @@ from..environments import DiscreteAtariEnv
 
 def build_ddqn_model(config: DiscreteActionConfig):
     config.env = DiscreteAtariEnv(config)
-    config.n_actions = config.env.get_n_actions()
+    f_channel, height, width, n_actions, n_channel = config.env.get_shape()
+    config.n_actions = n_actions
+    config.height = height
+    config.width = width
+    config.memory_channel = n_channel
+    config.frame_channel = f_channel
+    config.input_channel = n_channel - f_channel
     config.eval_env = DiscreteAtariEnv(config, eval=True)
-    config.agent = DDQNAgent(config)
+
     config.memory = Replay(config)
-    config.input_channel = 5
-    config.writer = SummaryWriter(config.writer_name)
+
+    config.agent = DDQNAgent(config)
+
+    config.writer = SummaryWriter(f'runs/{config.writer_name}')
     config.action_selector = DiscreteActionSelector(config)
+
     train_atari(config)
